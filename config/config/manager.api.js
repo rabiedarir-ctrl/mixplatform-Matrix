ce047edd8907843e0ffd030932d425656d109d50
Mix platform <259675695+rabiedarir-ctrl@users.noreply.github.com>
2026-04-02 15:22:14 +0100

    Update manager.api.js

--- config/config/manager.api.js
+++ config/config/manager.api.js
@@ -1,6 +1,6 @@
 // config/manager.api.js
 
-const MANAGER_API_BASE = "https://rabiedarir-ctrl.github.io/mixplatform-Matrix/manager";
+const MANAGER_API_BASE = "http://localhost:3000";
 
 /**
  * تنفيذ طلب API لمدير الأعمال
