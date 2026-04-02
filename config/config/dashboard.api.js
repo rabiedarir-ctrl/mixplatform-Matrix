e6778ef6d577094cb74e6859b8440f9ef70caa74
Mix platform <259675695+rabiedarir-ctrl@users.noreply.github.com>
2026-04-02 15:37:18 +0100

    Update dashboard.api.js

--- config/config/dashboard.api.js
+++ config/config/dashboard.api.js
@@ -1,6 +1,6 @@
 // config/dashboard.api.js
 
-const API_BASE = "http://localhost:3000"; // غيّر الرابط حسب إعداداتك
+const API_BASE = "http://localhost:3000/api/dashboard"; // غيّر الرابط حسب إعداداتك
 
 /**
  * دالة fetch عامة للتواصل مع API
